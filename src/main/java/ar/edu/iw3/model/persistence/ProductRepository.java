26c29842221d8286746219fc6f888508da95c5dc
BrunoCavazza <bruno.boomvazza@gmail.com>
2024-09-03 16:34:43 -0300

    starup

---
+++ src/main/java/ar/edu/iw3/model/persistence/ProductRepository.java
@@ -0,0 +1,15 @@
+package ar.edu.iw3.model.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.iw3.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	Optional<Product> findByProduct(String product);
+
+	Optional<Product> findByProductAndIdNot(String product, long id);
+}
